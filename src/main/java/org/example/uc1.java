24972124a32445b8dfcc7bb63e0326c01028787d
ssshashank2007-cmd <ssshashank2007@gmial.com>
2026-04-20 13:34:40 +0530

    uc1

---
+++ src/main/java/org/example/uc1.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class uc1 {
+}
+public class UC1DisplayBoard {
+    public static char[][] createBoard() {
+        char[][] board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+        return board;
+    }
+
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] board = createBoard();
+        printBoard(board);
+    }
+}
